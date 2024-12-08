45db223b2cf5cc8807438970ffb3779d4c77a384
Amar Dahake <35018901+user97116@users.noreply.github.com>
2024-12-08 12:36:06 +0530

    Update RemoveViewFactory.swift

--- ios/Classes/views/RemoveViewFactory.swift
+++ ios/Classes/views/RemoveViewFactory.swift
@@ -3,8 +3,14 @@ import Flutter
 import UIKit
 
 class RemoveViewFactory:  NSObject, FlutterPlatformViewFactory {
+    private var view: UIView;
+    
+    init(view: UIView) {
+        self.view = view
+    }
+    
     func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> any FlutterPlatformView {
-        <#code#>
+        return RemoteView(testView: view)
     }
     
 }
