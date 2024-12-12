b66856811890dad365306fd37d6b17908829d013
Amar Dahake <35018901+user97116@users.noreply.github.com>
2024-12-12 15:28:58 +0530

    Added strategy

--- ios/Classes/streams/strategy/PublishStrategy.swift
+++ ios/Classes/streams/strategy/PublishStrategy.swift
@@ -1,7 +1,1 @@
-//
-//  PublishStrategy.swift
-//  Pods
-//
-//  Created by Amar on 12/12/24.
-//
 
--- ios/Classes/streams/strategy/ViewerStrategy.swift
+++ ios/Classes/streams/strategy/ViewerStrategy.swift
@@ -1,5 +1,25 @@
 import AmazonIVSBroadcast
 
-public class ViewerStrategy {
+public class ViewerStrategy: NSObject, IVSStageStrategy {
+    public func stage(_ stage: IVSStage, streamsToPublishForParticipant participant: IVSParticipantInfo) -> [IVSLocalStageStream] {
+        return [];
+    }
     
+    public func stage(_ stage: IVSStage, shouldPublishParticipant participant: IVSParticipantInfo) -> Bool {
+        return false
+    }
+    
+    public func stage(_ stage: IVSStage, shouldSubscribeToParticipant participant: IVSParticipantInfo) -> IVSStageSubscribeType {
+        return IVSStageSubscribeType.audioVideo
+    }
+    
+    public func stage(_ stage: IVSStage, subscribeConfigurationForParticipant participant: IVSParticipantInfo) -> IVSSubscribeConfiguration {
+        var config = IVSSubscribeConfiguration();
+        do {
+            try config.jitterBuffer.setMinDelay(IVSJitterBufferDelay.medium())
+        } catch {
+            print("Error setting jitter buffer delay: \(error)")
+        }
+        return config
+    }
 }
